aa8ba64e7de94f310b3133fa147bdfd354310e84
Lasmaroussema <lasmaroussama88@gmail.com>
2023-08-08 14:27:30 +0100

    5th project

---
+++ draw.js
@@ -0,0 +1,73 @@
+let can=document.getElementById("canvas");
+let ctx=can.getContext("2d");
+let canpos = can.getBoundingClientRect();
+ctx.lineCap = 'round';
+let drawing = false;
+
+
+
+
+document.querySelectorAll("li").forEach(function(e)
+    {
+        console.log(e);
+        e.style.color=e.innerText;
+    })
+let colors=["blue","yellow","red","green","orange","pink","purple","white","brown","aqua","chartreuse"];
+let r;
+
+
+
+
+
+    function init(event) {
+        drawing = true;
+        ctx.beginPath();
+        ctx.moveTo(event.clientX - canpos.left, event.clientY - canpos.top);
+    }
+    function draw(event) {
+        if (!drawing)
+            return;
+        ctx.lineWidth = 7;
+        ctx.lineTo(event.clientX - canpos.left, event.clientY - canpos.top);
+        ctx.moveTo(event.clientX - canpos.left, event.clientY - canpos.top);
+        ctx.strokeStyle = colors[r];
+        ctx.stroke();
+    }
+    function stop() {
+        drawing = false;
+        ctx.closePath();
+    }
+
+
+
+
+
+document.getElementById("draw").onclick=function()
+{
+    if(document.querySelector(".msg").innerText!="")
+{
+    return;
+}
+document.getElementById("clear").style.display="block";
+    document.querySelector("ul").style.display="block";
+    document.querySelector(".msg").appendChild(document.createTextNode(" choose color , click and drag to draw , relach to stop : "));
+    document.querySelectorAll("li").forEach(function(e)
+    {
+        e.onclick=function()
+        {
+            r=e.getAttribute("id");
+        }
+    })
+     can.addEventListener("mousedown", init);
+        can.addEventListener("mousemove", draw);
+        can.addEventListener("mouseup", stop);
+};
+
+
+
+
+
+document.getElementById("clear").onclick=function()
+{
+  ctx.clearRect(0,0,can.width,can.height);
+}
